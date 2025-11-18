f6e90a7c94e4b2cdae867adab45d93385b6f5f13
Maqsudbek <mahmudovmaqsudbek2607@gmail.com>
2025-11-19 02:01:24 +0500

    Updated FilterPage

--- test/pageobjects/filter.page.js
+++ test/pageobjects/filter.page.js
@@ -2,21 +2,24 @@ import BasePage from "./base.page";
 
 class FilterPage extends BasePage {
 
-    get allMenuButton() { return $("div.sc-8798a0ad-0"); }
-    //get allMenuButton() { return $("button[aria-label='Menu']"); }
-    get top250MoviesLink() { return $("//span[contains(text(),'Top 250 movies')]"); }
-    get filterButton() { return $("[data-testid='filter-menu-button']"); }
-    get closeButton() { return $(".ipc-promptable-base__close"); }
-    get minRatingInput() { return $("[name='filter-user-rating-start-input']"); }
+    get allMenuButton() { return $("div.sc-8798a0ad-0") }
+    get top250MoviesLink() { return $("//span[contains(text(),'Top 250 movies')]") }
+    get filterButton() { return $("[data-testid='filter-menu-button']") }
+    get closeButton() { return $(".ipc-promptable-base__close") }
+    get minRatingInput() { return $("[name='filter-user-rating-start-input']") }
     get maxRatingInput() { return $("[name='filter-user-rating-end-input']") }
-    get minVoteInput() { return $("[name='filter-user-rating-count-start-input']"); }
-    get maxVoteInput() { return $("[name='filter-user-rating-count-end-input']"); }
-    get moviesList() { return $$(".ipc-metadata-list li"); }
-    get movieRatingSelector() { return ".ipc-metadata-list-summary-item__c .ipc-rating-star .ipc-rating-star--rating";}
-    get numberOfVotesSelector() {return ".ipc-metadata-list-summary-item__c .ipc-rating-star .ipc-rating-star--voteCount";}
-    get sortDropdown() { return $("#sort-by-selector"); }
-    get sortOptionUserRating() { return $("#sort-by-selector option[value='USER_RATING']"); }
-    get descendingButton() { return $("#swap-sort-order-button"); }
+    get minVoteInput() { return $("[name='filter-user-rating-count-start-input']") }
+    get maxVoteInput() { return $("[name='filter-user-rating-count-end-input']") }
+    get moviesList() { return $$(".ipc-metadata-list li") }
+    get movieRatingSelector() { return ".ipc-metadata-list-summary-item__c .ipc-rating-star .ipc-rating-star--rating"}
+    get numberOfVotesSelector() {return ".ipc-metadata-list-summary-item__c .ipc-rating-star .ipc-rating-star--voteCount"}
+    get sortDropdown() { return $("#sort-by-selector") }
+    get sortOptionUserRating() { return $("#sort-by-selector option[value='USER_RATING']") }
+    get descendingButton() { return $("#swap-sort-order-button") }
+    
+    get firstMovieTitle() { 
+        return this.moviesList[0].$("a .ipc-title__text");
+    }
 
     getResult(index) {
         return this.moviesList[index];
@@ -44,7 +47,6 @@ class FilterPage extends BasePage {
     async setUserRatingFilter(min, max) {
         await this.minRatingInput.scrollIntoView();
         await this.type(this.minRatingInput, min);
-
         await this.maxRatingInput.scrollIntoView();
         await this.type(this.maxRatingInput, max);
     }
@@ -52,7 +54,6 @@ class FilterPage extends BasePage {
     async setUserVoteFilter(min, max) {
         await this.minVoteInput.scrollIntoView();
         await this.type(this.minVoteInput, min);
-
         await this.maxVoteInput.scrollIntoView();
         await this.type(this.maxVoteInput, max);
     }
@@ -61,23 +62,23 @@ class FilterPage extends BasePage {
         await this.click(this.closeButton);
         await browser.pause(500);
     }
+
     async sortByImdbRating() {
-
-        const dropdown = await this.sortDropdown;
-
-        await dropdown.waitForDisplayed();
-        await dropdown.waitForClickable();
-        await dropdown.scrollIntoView();
-
-        await dropdown.click();
-
-        const option = await this.sortOptionUserRating;
-        await this.click(option);
-        return true;
+        this.click(await this.sortDropdown);
+        await this.sortOptionUserRating.click();
     }
 
     async changeSortedItemsDescendingOrder() {
-        await this.click(this.descendingButton);
+        this.click(await this.descendingButton);
+    }
+
+    async waitForFirstMovieToBe(title) {
+        await browser.waitUntil(
+            async () => {
+                const text = await this.firstMovieTitle.getText();
+                return text.includes(title);
+            }
+        );
     }
 }
 
