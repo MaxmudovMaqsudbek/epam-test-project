ef5d3bee13d7164cad49a66143d9fe46463a5bab
Maqsudbek <mahmudovmaqsudbek2607@gmail.com>
2025-12-16 13:44:06 +0500

    Added eslint and prettier

--- test/data/filter.data.js
+++ test/data/filter.data.js
@@ -1,11 +1,11 @@
 export default {
-  voteRange: {
-    min: 3_000_000,
-    max: 4_000_000
-  },
-  ratingRange: {
-    min: 9,
-    max: 10
-  },
-  expectedFirstMovie: "The Incredibles"
+    voteRange: {
+        min: 3_000_000,
+        max: 4_000_000,
+    },
+    ratingRange: {
+        min: 9,
+        max: 10,
+    },
+    expectedFirstMovie: 'The Incredibles',
 };
