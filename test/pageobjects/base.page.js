4e3f433ec94348b1cc5d441da0bf3a42b5cea88a
MaxmudovMaqsudbek <MaxmudovMaqsudbek@users.noreply.github.com>
2025-12-16 08:59:46 +0000

    deploy: 30d534beb8207dfc0b44b1df7f4f1ee6b3370cee

--- test/pageobjects/base.page.js
+++ test/pageobjects/base.page.js
@@ -1,22 +1,23 @@
-class BasePage {
-    open(path) {
+class BasePage{
+    open(path){
         return browser.url(path);
     }
 
-    async click(element) {
+    async click(element){
         await element.waitForDisplayed();
         await element.waitForClickable();
         await element.click();
     }
 
-    async type(element, value) {
+    async type(element, value){
         await element.waitForDisplayed();
         await element.setValue(value);
     }
 
-    async wait(element) {
+    async wait(element){
         await element.waitForDisplayed();
     }
+
 }
 
 export default BasePage;
