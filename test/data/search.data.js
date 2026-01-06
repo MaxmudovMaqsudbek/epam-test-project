901c82bf50c1fed0310e7cf2ce02d88fb5788baa
MaxmudovMaqsudbek <MaxmudovMaqsudbek@users.noreply.github.com>
2026-01-06 14:29:22 +0000

    deploy: 4a4bb35c7212ecb2237d0a61ce7cb6111c2eab58

---
+++ test/data/search.data.js
@@ -0,0 +1,8 @@
+export default {
+    correctMovie: 'Home Alone',
+    nonExistentMovie: 'Back to the Future',
+    commonMovie: 'Batman',
+    sciFiMovie: 'Matrix',
+    gibberish: 'thjsdhfjhwfjkewjkjfek',
+    notFoundMessage: 'No results found for',
+};
