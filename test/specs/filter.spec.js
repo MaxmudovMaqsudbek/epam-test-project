a058927063ca5ef5e492754524965bf9a362096f
MaxmudovMaqsudbek <MaxmudovMaqsudbek@users.noreply.github.com>
2026-03-25 07:09:21 +0000

    deploy: 4a4bb35c7212ecb2237d0a61ce7cb6111c2eab58

---
+++ test/specs/filter.spec.js
@@ -0,0 +1,48 @@
+import { expect } from 'chai';
+import filterPage from '../pageobjects/filter.page';
+import filterData from '../data/filter.data';
+
+describe('Filter Functionality Spec', () => {
+    beforeEach(async () => {
+        await filterPage.open('https://www.imdb.com/');
+    });
+    it('should filter movie by User Rating', async () => {
+        await filterPage.openTop250Movies();
+        await filterPage.openFilterMenu();
+        await filterPage.setUserRatingFilter(
+            filterData.ratingRange.min,
+            filterData.ratingRange.max
+        );
+        await filterPage.closeFilterMenu();
+        const movies = await filterPage.moviesList;
+        expect(movies.length).to.be.greaterThan(0);
+        await filterPage.validateRatingRange(
+            filterData.ratingRange.min,
+            filterData.ratingRange.max
+        );
+    });
+    it('should filter movies by Number of Votes', async () => {
+        await filterPage.openTop250Movies();
+        await filterPage.openFilterMenu();
+        await filterPage.setUserVoteFilter(
+            filterData.voteRange.min,
+            filterData.voteRange.max
+        );
+        await filterPage.closeFilterMenu();
+        const movies = await filterPage.moviesList;
+        expect(movies.length).to.be.greaterThan(0);
+        await filterPage.validateVotesAreWithinRange(
+            filterData.voteRange.min,
+            filterData.voteRange.max
+        );
+    });
+    it('should check the first movie when IMDb rating is sorted in descending order', async () => {
+        await filterPage.openTop250Movies();
+        await filterPage.sortByImdbRating();
+        await filterPage.changeSortedItemsDescendingOrder();
+        await browser.pause(500);
+        await filterPage.waitForFirstMovieToBe(filterData.expectedFirstMovie);
+        const movies = await filterPage.moviesList;
+        expect(movies.length).to.be.greaterThan(0);
+    });
+});
